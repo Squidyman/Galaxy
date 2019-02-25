dd6b902b12fb241a858248adeeb258046ebfadf0
Matthew <mlbarnard1@cougars.ccis.edu>
2019-02-25 17:14:32 +0000

    Dylan version

--- flask_app.py
+++ flask_app.py
@@ -25,6 +25,17 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
